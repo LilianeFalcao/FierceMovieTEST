717c96f06c358d04110e64084c4f73d30389e3b6
Giovanni <giovannisouza216@gmail.com>
2022-10-13 16:02:43 -0300

    estrelinha

--- js/script.js
+++ js/script.js
@@ -31,3 +31,16 @@ controls.forEach((control) => {
     items[currentItem].classList.add("current-item");
   });
 });
+
+var stars = document.querySelectorAll('.star-icon');
+                  
+document.addEventListener('click', function(e){
+  var classStar = e.target.classList;
+  if(!classStar.contains('ativo')){
+    stars.forEach(function(star){
+      star.classList.remove('ativo');
+    });
+    classStar.add('ativo');
+    console.log(e.target.getAttribute('data-avaliacao'));
+  }
+});
